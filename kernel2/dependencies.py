83cd27f4d2183d0433c2d7738cb1457b15496ec5
Shivank Goel <shivankgoel@dhcp-hol-2108.redrover.cornell.edu>
2019-04-20 16:48:59 -0400

    kernel 2 done

--- kernel2/dependencies.py
+++ kernel2/dependencies.py
@@ -17,3 +17,5 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+
+import gensim
--- kernel2/multicnnoriginal.py
+++ kernel2/multicnnoriginal.py
@@ -2,38 +2,37 @@ from dependencies import *
 from loaddata import *
 
 
-class mlpmodel(nn.Module):
+class multichannelcnnmodel(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.dropout1 = nn.Dropout(p=0.5)
-		self.fc1 = nn.Linear(11*300,10)
+		self.dropout2 = nn.Dropout(p=0.5)
+		self.conv1 = nn.Conv1d(300, 10, 3)
+		#output is 10*9
+		self.pool1 = nn.MaxPool1d(9,1)
+		#output is 10*1
+		self.conv2 = nn.Conv1d(300,10,5)
+		#output is 10*7
+		self.pool2 = nn.MaxPool1d(7, 1)
+		#output is 10*1acc
 		self.fc2 = nn.Linear(45,10)
-		self.bn1 = nn.BatchNorm1d(50)
-		self.fc3 = nn.Linear(50,60)
+		self.bn1 = nn.BatchNorm1d(80)
+		self.fc3 = nn.Linear(80,60)
 		self.fc4 = nn.Linear(60,3)
 	def forward(self,embeddings,positions):
-		e1 = [self.dropout1(embedding) for embedding in embeddings]
+		a_s = [self.conv1(self.dropout1(embedding.transpose(1, 2))) for embedding in embeddings]
+		a = [self.pool1(t).transpose(1,2).flatten(1) for t in a_s]
+		b_s = [self.conv2(self.dropout2(embedding.transpose(1, 2))) for embedding in embeddings]
+		b = [self.pool2(t).transpose(1, 2).flatten(1) for t in b_s]
+		e1 = [torch.cat((a[i],b[i]),1) for i in range(3)]
 		e1 = [e.flatten(1) for e in e1]
-		x1 = torch.cat(tuple([self.fc1(e) for e in e1]),1)
+		x1 = torch.cat(tuple([e for e in e1]),1)
 		x2 = torch.cat(tuple([self.fc2(position) for position in positions]),1)
 		x = F.relu(torch.cat((x1,x2),1))
 		xnorm = self.bn1(x)
 		hiddenlayer = F.relu(self.fc3(xnorm))
 		outs = F.softmax(self.fc4(hiddenlayer),dim=1)
 		return outs
-
-
-'''
-Test data
-
-a = torch.tensor(p_emb_dev,dtype=torch.float)
-b = torch.tensor(a_emb_dev,dtype=torch.float)
-c = torch.tensor(b_emb_dev,dtype=torch.float)
-d = torch.tensor(pa_pos_dev,dtype=torch.float)
-e = torch.tensor(pb_pos_dev,dtype=torch.float)
-
-outs = mod([a,b,c],[d,e])
-'''
 
 
 dev_data = [torch.tensor(x,dtype=torch.float) for x in [p_emb_dev,a_emb_dev,b_emb_dev,pa_pos_dev,pb_pos_dev]]
@@ -45,12 +44,11 @@ y_dev = torch.tensor(y_dev)
 y_test = torch.tensor(y_test)
 
 
-
-model = mlpmodel()
+model = multichannelcnnmodel()
 opt = torch.optim.Adam(model.parameters())
 criterion = nn.CrossEntropyLoss()
 
-num_epochs = 1000
+num_epochs = 20
 
 for i in range(num_epochs):
 	opt.zero_grad()
@@ -65,20 +63,9 @@ for i in range(num_epochs):
 model.eval()
 dev_outputs = model([dev_data[0],dev_data[1],dev_data[2]],[dev_data[3],dev_data[4]])
 predicted_labels = torch.max(dev_outputs,1)[1]
-
-sum = 0
-predvals = np.array(predicted_labels)
-truevals = np.array(y_dev)
-for i in range(len(predvals)):
-	if predvals[i] == truevals[i]:
-		sum+=1
-
-acc = sum/len(predvals)
-
-#savethemodel
-# def save_checkpoint(filename, **states):
-#    torch.save(states, filename)
-# utils.save_checkpoint('model_mlp.pth.tar', **{'model' : model.state_dict(), 'opt' : opt.state_dict()})
+acc = np.sum(np.array(predicted_labels) == y_dev.values)/len(y_dev)
 
 
 
+
+#https://www.kaggle.com/keyit92/coref-by-mlp-cnn-coattention/data
